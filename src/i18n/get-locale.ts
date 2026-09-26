91be2d358016a82b9602b81420de5cd3bb4ba955
Tanya Fedchenko <30293847+fetchenko@users.noreply.github.com>
2026-09-26 11:49:39 +0300

    Simplify locale detection

--- src/i18n/get-locale.ts
+++ src/i18n/get-locale.ts
@@ -8,45 +8,19 @@ function localeFromLanguageTag(languageTag: string): Locale | null {
   return isLocale(language) ? language : null;
 }
 
-function getLocaleFromAcceptLanguage(
-  acceptLanguage: string | null,
-): Locale | null {
-  if (!acceptLanguage) {
-    return null;
+export async function getLocale(): Promise<Locale> {
+  const cookieLocale = (await cookies()).get("locale")?.value;
+
+  if (cookieLocale && isLocale(cookieLocale)) {
+    return cookieLocale;
   }
 
-  for (const languageRange of acceptLanguage.split(",")) {
-    const language = languageRange.split(";")[0];
-    const locale = localeFromLanguageTag(language);
-
-    if (locale) {
-      return locale;
-    }
-  }
-
-  return null;
-}
-
-export function resolveLocale(
-  savedLocale: string | undefined,
-  acceptLanguage: string | null,
-): Locale {
-  if (savedLocale && isLocale(savedLocale)) {
-    return savedLocale;
-  }
+  const browserLocale = (await headers())
+    .get("accept-language")
+    ?.split(",")[0];
 
   return (
-    getLocaleFromAcceptLanguage(acceptLanguage) ??
+    (browserLocale && localeFromLanguageTag(browserLocale)) ??
     defaultLocale
-  );
-}
-
-export async function getLocale(): Promise<Locale> {
-  const cookieStore = await cookies();
-  const requestHeaders = await headers();
-
-  return resolveLocale(
-    cookieStore.get("locale")?.value,
-    requestHeaders.get("accept-language"),
   );
 }
